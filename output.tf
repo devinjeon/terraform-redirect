fba130a13731b0f85585f4a0420a390ebc19070d
devinjeon <gywns2702@gmail.com>
2018-12-07 02:58:41 +0900

    Format

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "target_domain_name" {
-  value = "${aws_api_gateway_domain_name.links.regional_domain_name}"
+  value       = "${aws_api_gateway_domain_name.links.regional_domain_name}"
   description = "The hostname for the custom domain's regional endpoint."
 }
